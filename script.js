1c84d799a8acdacebd6011529a50c3147ab4e00a
Rohan <rohan6350458524@gmail.com>
2026-05-24 14:31:53 +0530

    Updated portfolio responsive design and UI improvements

--- script.js
+++ script.js
@@ -75,3 +75,26 @@ setInterval(function(){
         index = 0;
     }
 },2000);
+
+const menuToggle = document.querySelector(".menu-toggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+const navItems = document.querySelectorAll(".nav-links a");
+
+navItems.forEach((item) => {
+
+    item.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
+
